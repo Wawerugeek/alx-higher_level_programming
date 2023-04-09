92e0d3bb31c80d8c17605365aa2e35a26e914fd5
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 01:06:20 +0300

    more

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""a function to input squares of a function"""
 
 
 def square_matrix_simple(matrix=[]):
-    """Initialize an empty list to store the values"""
     result_matrix = []
 
     for row in matrix:
