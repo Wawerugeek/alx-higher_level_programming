5e473d70fdc29640e08ed71e05dd05e6d895ee22
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 00:55:10 +0300

    squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    result_matrix = []
+
+    for row in matrix:
+        result_row = []
+        for cols in row:
+            mult =cols * cols
+            result_matrix.append(mult)
+        result_matrix.append(result_row)
+
+    return result_matrix
