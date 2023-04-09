63524e9db52d7d6c19b48f162f33f6d9975c63d6
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 01:42:50 +0300

    unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,8 @@
 
 def uniq_add(my_list=[]):
     new_list = set(my_list)
+    count = 0
     for x in new_list:
-        new_list.append(x += 1)
+        count += x
 
-        return new_list
+    return count
