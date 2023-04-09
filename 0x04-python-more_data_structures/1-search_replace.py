33d87b153ce0ce200cc83ac61621b723b9fe4c56
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 01:26:16 +0300

    add

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+
+    return new_list
