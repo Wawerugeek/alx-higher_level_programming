534e36188ea17ab5e61d074e7358b6e1570287d0
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-11 00:32:43 +0300

    setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define class square:
+     Attributes:
+     size: Private instance attribute: size:
+"""
+
+
+class Square:
+    """class that has an attribute of size"""
+    def __init__(self, size=0):
+        """creates a new instance and set it to private
+         Args:
+        size: the side of the square
+         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @propety
+    def size(self):
+        """getter method for size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current aree"""
+        return (self.__size * self.__size)
