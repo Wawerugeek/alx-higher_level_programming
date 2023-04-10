74cb85bd49c14b42065e5cd41d54d4edf976b802
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-11 00:17:52 +0300

    returns a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define class square:
+     Attributes:
+     size: Private instance attribute: size:
+"""
+
+
+class Square:
+    """class that has an attribute of size"""
+    def __init__(self, size=0):
+        """creates a new instance and set it to private
+         Args:
+        size: the side of the square
+         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """returns the current aree"""
+        return (self.__size * self.__size)
