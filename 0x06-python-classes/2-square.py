e8f25308f99e3759569bce4e30ae1ab4261dad99
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-11 00:05:50 +0300

    pycodestyle update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,8 +12,8 @@ class Square:
          Args:
         size: the side of the square
          """
-    if not isinstance(size, int):
-        raise TypeError(size must be an integer)
-    if size < 0:
-        raise ValueError(size must be >= 0)
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError(size must be an integer)
+        if size < 0:
+            raise ValueError(size must be >= 0)
+        self.__size = size
