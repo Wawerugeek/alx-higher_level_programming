9020891f3f60f229f8c7f6e98ee4e2f59a498e95
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 01:31:28 +0300

    obj is an inst

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module that returns True if the object is an instance of,
+or if the object is an instance of a class
+that inherited from, the specified class ; otherwise False."""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns: Bool --> True or false
+    args: obj, class"""
+    if isinstance(obj, a_class):
+        return True
