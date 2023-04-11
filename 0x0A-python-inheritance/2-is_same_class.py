e14ce32f31da7fd058a9aca2d0ec15bc01a2a843
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 01:25:05 +0300

    type(obj) is a_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,8 +5,7 @@
 def is_same_class(obj, a_class):
     """test whether its of the same instance
     Args: obj (object): the object to be checked
-    a_class: the class to compare with 
-
+    a_class: the class to compare with
     returns: bool true/false
     """
     return type(obj) is a_class
