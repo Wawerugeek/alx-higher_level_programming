d3a818191635781eca9f3ce2b1c2c87109d85eca
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 01:57:04 +0300

    improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to populate BaseGeometry"""
+
+
+class BaseGeometry:
+    """defines method area 
+    raises an Exception"""
+    def area(self):
+        raise Exception('area() is not implemeted')
