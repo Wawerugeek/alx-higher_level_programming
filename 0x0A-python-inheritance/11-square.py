e140aec96fc2423abbf61a4a5b0f3fe1d8e2ea34
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 04:11:05 +0300

    @__str__

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-"""class square that inherits from subclass rectangle"""
+"""a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """constructor with two arguments"""
+    def __init__(self, width, height):
+        """args:
+        width: int
+        height: int """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """implementation of the method area"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """returns string represenation of the rectangle"""
+        return (f"[Rectangle] {self.__width}/{self.__height}")
 
 
 class Square(Rectangle):
@@ -16,6 +36,6 @@ class Square(Rectangle):
         """implementation of the area method side squared"""
 
         return self.__size ** 2
-    
+
     def __str__(self):
         return (f"[Square] {self.__size}{self.__size}")
