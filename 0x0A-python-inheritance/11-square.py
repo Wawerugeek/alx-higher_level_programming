65f7a73c03f9dfb5e4d222f4746f77af1badd044
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 03:31:38 +0300

    str()

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""class square that inherits from subclass rectangle"""
+
+
+class Square(Rectangle):
+    """building its constructor"""
+    def __init__(self, size):
+        """method to initialize attributes"""
+
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """implementation of the area method side squared"""
+
+        return self.__size ** 2
+    
+    def __str__(self):
+        return (f"[Square] {self.__size}{self.__size}")
