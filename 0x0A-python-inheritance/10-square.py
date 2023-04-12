6618ec035d2cbf0dfa2cdffd986d3124eb970a9e
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-12 03:17:17 +0300

    changes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,10 +6,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """building its constructor"""
     def __init__(self, size):
-        self.integer_validator("size", size)
+        """method to initialize attributes"""
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
-    
+
     def area(self):
         """method area implementation"""
         return self.__size * self.__size
