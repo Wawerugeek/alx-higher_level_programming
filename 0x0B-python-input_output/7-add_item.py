dca4d8eda1044e20968fd0d68ee9716a453df1a4
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-13 05:09:53 +0300

    system

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,13 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_fi
 le
-if __name__ == "__main__":
-    try:
-        j_list = load_from_json_file('add_item.jsom')
-    except FileNotFoundError:
-        j_list = []
+try:
+    j_list = load_from_json_file('add_item.jsom')
+except FileNotFoundError:
+    j_list = []
 
-    for file in range(1, len(sys.argv)):
-        j_list.appenf(sys.argv[file])
-    save_to_json_file(j_list, "add_item.json")
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv):
+            j_list.append(sys.argv[i]
+
+save_to_json_file(j_list, "add_item.json")
