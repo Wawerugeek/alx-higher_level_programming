45ccefd43b5dcc19dab8d2151699cf06ef1bf709
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-13 03:21:51 +0300

    json.dumps(my_obj)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""module that return JSON representation of an object"""
 import json
-"""function that return JSON representation of an object"""
+"""module that will enable us to use json file"""
 
 
 def to_json_string(my_obj):
     """it will take python object and return json str rep"""
     return json.dumps(my_obj)
-
