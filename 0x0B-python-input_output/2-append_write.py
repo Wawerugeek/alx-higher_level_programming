b04b443573675ed8df481a589406715470a43262
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-13 03:08:53 +0300

    append a string to the end of the line

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that appends a string at the end"""
+
+
+def append_write(filename="",text=""):
+    """this will append at the end of the line"""
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
