40cf0bf683d69ed227c14ac661ea0cb39b7e67ac
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-13 05:40:45 +0300

    another approach

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,15 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = \
-        __import__('6-load_from_json_file.py').load_from_json_file
+if __name__ == "__main__":
+    """we import from  two modules"""
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    j_list = load_from_json_file('add_item.jsom')
-except FileNotFoundError:
-    j_list = []
-
-if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv):
-            j_list.append(sys.argv[i]
-
-save_to_json_file(j_list, "add_item.json")
+    try:
+        j_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        j_list = []
+    j_list.extend(sys.argv[1:])
+    save_to_json_file(j_list, "add_item.json")
