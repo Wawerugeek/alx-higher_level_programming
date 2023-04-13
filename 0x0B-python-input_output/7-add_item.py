738556a76ec6f4748040d71820acb109a9ca9c76
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-13 05:36:42 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,8 @@ import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+load_from_json_file = \
+        __import__('6-load_from_json_file.py').load_from_json_file
 
 try:
     j_list = load_from_json_file('add_item.jsom')
