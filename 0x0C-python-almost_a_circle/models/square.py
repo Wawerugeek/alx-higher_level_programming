154b686994cffccaa07c8f09b06b5fd6dcc406b2
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-18 00:34:30 +0300

    daily contribution

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,3 +23,29 @@ class Square(Rectangle):
         """the setter method"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """the update module that follows the following
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+        kwargs must be skipped if args exists
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        elif kwargs and not args:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.size = kwargs["x"]
+            if "y" in kwargs:
+                self.size = kwargs["y"]
