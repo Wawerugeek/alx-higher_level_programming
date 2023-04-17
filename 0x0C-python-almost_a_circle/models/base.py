1833153fcc0375ea5fc8faf830b9dc7893f000b4
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-18 01:18:41 +0300

    dictionary to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,8 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
+        """static method that converts from dictionary to json"""
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
