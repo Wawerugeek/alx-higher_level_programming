bb8e181b1672fe565601ec2df75718e717a45902
Wawerugeek <stevemuiruri84@gmail.com>
2023-05-16 22:46:41 +0300

    changes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
---script that list the number of records
+-- script that list the number of records
 
 SELECT `score`, COUNT(*) as `number`
 FROM `second_table`
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
