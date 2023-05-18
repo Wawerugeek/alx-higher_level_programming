c648b1aae9afd8e1b03c615ec0f6e8b4a5cd68ca
Wawerugeek <stevemuiruri84@gmail.com>
2023-05-18 05:16:24 +0300

    updates

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,8 @@
 -- joins info from two tables
 
+
 select tv_shows.title, tv_show_genres.genre_id from tv_shows
 inner join tv_show_genres 
 on tv_shows.id = tv_show_genres.show_id 
 order by tv_shows.title, tv_show_genres.genre_id asc;
+
