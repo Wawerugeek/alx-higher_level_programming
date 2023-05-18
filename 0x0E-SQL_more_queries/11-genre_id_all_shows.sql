6652dc94a0d3d7de6f47c25f13b21f7bc617391b
Wawerugeek <stevemuiruri84@gmail.com>
2023-05-18 05:20:58 +0300

    inner join'

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- import db dump into hbtn_od_tvshow
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
