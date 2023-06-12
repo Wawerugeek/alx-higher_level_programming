e05eeb78ed69585f32145edc19cabcc7237a1392
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-13 00:18:50 +0300

    Advanced tasks

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () { return ++myObject.value; };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
