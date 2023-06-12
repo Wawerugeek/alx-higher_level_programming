6272c26abde2d5a57556c42b77da5304fc176c7e
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-13 00:33:00 +0300

    output update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const str = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < str.length; i++) {
   console.log(str[i]);
 }
