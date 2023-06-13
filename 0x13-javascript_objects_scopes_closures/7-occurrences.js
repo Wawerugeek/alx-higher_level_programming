172e360e4de0f97cabc1e2492037038311f5eb8f
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-13 21:24:26 +0300

    occurences fixing

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    list[i] === searchElement ? count++ : '';
+    if (list[i] === searchElement) {
+      count++;
+    }
   }
   return count;
 };
