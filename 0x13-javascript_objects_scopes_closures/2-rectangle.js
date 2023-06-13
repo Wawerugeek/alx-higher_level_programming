88006b5e82c91a083e0baa8813949ebe219ccff2
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-13 20:25:53 +0300

    conditions to the constructor

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
