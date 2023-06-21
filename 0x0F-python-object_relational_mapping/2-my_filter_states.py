23ab2b1489cbe1465dcc3edf48dca3c9f33fdb6a
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-21 11:14:09 +0300

    handle user input as command line argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"filter states by user input"
+
+import sys
+import MySQLdb
+
+
+def main():
+    """ This function filters states by user input
+    """
+    adm = sys.argv[1]
+    pw = sys.argv[2]
+    name = sys.argv[3]
+    state = sys.argv[4]
+    db = MySQLdb.connect(
+        host="localhost",
+        user=adm,
+        passwd=pw,
+        db=name,
+        port=3306
+        )
+    cur = db.cursor()
+    query = ("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state))
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
