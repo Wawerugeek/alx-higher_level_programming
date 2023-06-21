d2872bf9d0c58ac672c72775b592ab13b341b8c8
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-21 10:30:49 +0300

    pep8 compliance in first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,29 @@
 import sys
 import MySQLdb
 
+
 def main():
     """ This function searches all the cities
     """
     adm = sys.argv[1]
     pw = sys.argv[2]
     name = sys.argv[3]
-    
-    db = MySQLdb.connect(host="localhost", user= adm, passwd=pw, db=name, port=3306)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=adm,
+        passwd=pw,
+        db=name,
+        port=3306
+        )
     cur = db.cursor()
     query = "SELECT * FROM states ORDER BY states.id ASC"
     cur.execute(query)
-    
     row = cur.fetchall()
-    
     for r in row:
         print(r)
     cur.close()
     db.close()
-    
+
+
 if __name__ == "__main__":
     main()
