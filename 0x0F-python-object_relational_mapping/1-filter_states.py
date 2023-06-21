084d287616d44296a78476091d7182317896d06e
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-21 11:00:16 +0300

    LIKE - matching patterns BINARY- case senstive

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"prints all the states from the database"
+
+import sys
+import MySQLdb
+
+
+def main():
+    """ This function searches all the cities
+    """
+    adm = sys.argv[1]
+    pw = sys.argv[2]
+    name = sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        user=adm,
+        passwd=pw,
+        db=name,
+        port=3306
+        )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%'" \
+        "ORDER BY states.id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
