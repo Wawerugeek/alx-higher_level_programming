08aa80bb9907165eec887304278528ebd3ad5141
Wawerugeek <stevemuiruri84@gmail.com>
2023-06-22 23:18:54 +0300

    sql injection free code

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" prints state objects with letter a
+    """
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """for printing state objects with letter a"""
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user, password, db_name))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like(state_name)):
+        print(f"{state.id}")
+    if (not state):
+        print("Not Found")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
