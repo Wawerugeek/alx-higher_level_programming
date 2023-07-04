a717c4602ea9f734ea740a94a3c6cf4a4a728bf6
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-04 21:07:31 +0300

    Response header value:

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""_a script that:
+    takes in a URL
+    sends a request to the url and displays the value
+    _
+    """
+import sys
+from urllib import request
+
+
+def main():
+
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        data = response.info()
+        print(data.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    main()
