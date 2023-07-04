497de7bcc22e719248aba871f4cedc6de1e6d969
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-05 00:45:35 +0300

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,10 +7,10 @@ import requests
 
 def main():
     url = "http://0.0.0.0:5000/search_user"
-    if (len(sys.argv) > 1):
-        var = sys.argv[1]
-    else:
+    if (len(sys.argv) < 2):
         var = ""
+    else:
+        var = sys.argv[1]
     data = {"q": var}
     response = requests.post(url, data=data)
     try:
@@ -18,7 +18,7 @@ def main():
         if not j_form or len(j_form) == 0:
             print("No result")
         else:
-            print(f"[{j_form.get('id')}], {j_form.get('name')}")
+            print(f"[{j_form.get('id')}] {j_form.get('name')}")
     except ValueError:
         print("Not a valid JSON")
 
