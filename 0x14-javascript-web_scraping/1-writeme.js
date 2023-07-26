ddea42d0bfd3024d23377fbf33f410b4f6c4091c
wawerugeek <stevemuiruri84@gmail.com>
2023-07-26 22:29:54 +0300

    write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
-const files = process.argv.slice(2)
-fs.write(files[0], files[1], (err) => {
-    if (err) console.log('error', err);
-})
+const fs = require('fs');
+const files = process.argv.slice(2);
+fs.writeFile(files[0], files[1], (err) => {
+  if (err) console.log('error', err);
+});
