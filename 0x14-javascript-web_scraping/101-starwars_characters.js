e4d46cf58da3f5f53965dd54eb72c2902f68c5c2
Wawerugeek <stevemuiruri84@gmail.com>
2024-06-14 10:21:11 +0530

    starwars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const files = process.argv.slice(2);
-const dct = {};
-let data;
-request(files[0], function (error, res, body) {
-  if (error) console.error('error:', error);
-  else {
-    data = JSON.parse(body);
-    for (const i in data) {
-      if (data[i].completed === true) {
-        if (!(data[i].userId in dct)) { dct[data[i].userId] = 1; } else { dct[data[i].userId] += 1; }
-      }
-    }
-    console.log(dct);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
